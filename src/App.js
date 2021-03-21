f1f5d7013115f1266654e7aa5abc9aa480fc9365
Karl Tulio <kltulio@up.edu.ph>
2021-03-21 16:34:29 -0400

    3rd commit
    Working table. Lacks styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Table from './components/Table';
+import  employees from './employees.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          sorted: false,
+          filtered: false,
+          employees
+      }
+      this.handleSortClick = this.handleSortClick.bind(this);
+      this.handleFilterClick = this.handleFilterClick.bind(this);
+  }
+  handleSortClick() {
+      // run only if sorted false
+      if (!this.state.isSorted) {
+          console.log("Sorted via last name")
+          const employees = this.state.employees.slice().sort((x, y) => {
+              if (x.last < y.last) return -1;
+              else if (x.last > y.last) return 1;
+              else return 0;
+          });
+          this.setState(state => ({
+              employees
+          }));
+      } else {
+          console.log("Data unsorted")
+          this.setState(state => ({
+              employees
+          }));
+      }
+      this.setState(state => ({
+          isSorted: !state.isSorted,
+      }));
+  }
+  handleFilterClick() {
+      // only filter when unfiltered
+      if (!this.state.isFiltered) {
+          const employees = this.state.employees.filter(employee => employee.email.includes(".org"));
+          this.setState(state => ({
+              employees
+          }));
+      } else {
+          this.setState(state => ({
+              employees
+          }));
+      }
+      this.setState(state => ({
+          isFiltered: !state.isFiltered,
+      }));
+  }
+  render() {
+      return (
+        <div>
+             <h1>Employee Directory</h1>
+              <div className="btn-group mb-3">
+                  <button type="button" datatoggle="tooltip" title="By last name" className="btn btn-primary btn-outline-dark" onClick={this.handleSortClick}>
+                      {this.state.isSorted ? 'Show' : 'Sort'}
+                  </button>
+                  <button type="button" datatoggle="tooltip" title="For @gmail.com" className="btn btn-primary btn-outline-dark" onClick={this.handleFilterClick}>
+                      {this.state.isFiltered ? 'Show' : 'Filter'}
+                  </button>
+              </div>
+              <Table slaveArray={this.state.employees}></Table>
+        </div>
+           
+      );
+  }
 }
 
 export default App;
